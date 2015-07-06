6dbb6b0946393a6f27a6f2b0146859a947bbae5a
ovidiuionut94 <pascaionut94@yahoo.com>
2015-07-06 18:23:11 +0300

    Problem 9-Start

---
+++ Problem9_SecretSanta/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem9_SecretSanta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            t1.ReadOnly = true;
+            t2.ReadOnly = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Person> personArray = new List<Person>();
+            StreamReader readFile = new StreamReader("Fisier.txt");
+            var line = "";
+            while (line != null)
+            {
+                line = readFile.ReadLine();
+                if (line == null)
+                    break;
+                var names = line.Split(' ');
+
+                Person newPerson = new Person();
+                newPerson.FirstName = names[0];
+                newPerson.SecondName = names[1];
+                personArray.Add(newPerson);
+
+                t1.AppendText(line + "\r\n");
+            }
+        }
+    }
+}
---
+++ Problem9_SecretSanta/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9_SecretSanta
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
