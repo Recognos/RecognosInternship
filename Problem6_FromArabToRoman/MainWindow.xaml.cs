2f38ccec0d5a422339627f477570a47546b3dca3
ovidiuionut94 <pascaionut94@yahoo.com>
2015-07-06 17:47:36 +0300

    Finished problem 6

---
+++ Problem6_FromArabToRoman/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Problem6_FromArabToRoman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            t2.IsReadOnly = true;
+            myButton.Click += myButton_Click;
+        }
+     
+
+        void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            var textToTransform = t1.Text;
+            try
+            {
+                int myInteger = Int32.Parse(textToTransform);
+                string result=Class1.TransformToRoman(myInteger);
+                t2.Text = result;
+            }
+            catch (ArgumentNullException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { }
+        }
+    }
+}
